bdc43a64dba30510d6608250a749f24119854ae8
wj19982021-dev <wj19982021-dev@users.noreply.github.com>
2026-06-10 02:29:07 +0800

    🧪 临时移除 LoadingScreen 测试

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import LoadingScreen from './components/LoadingScreen'
 import MusicToggle from './components/MusicToggle'
 import PetalLayer from './components/PetalLayer'
 import SceneIntro from './components/SceneIntro'
@@ -10,29 +9,23 @@ import SceneLetter from './components/SceneLetter'
 import './App.css'
 
 function App() {
-  const [loaded, setLoaded] = useState(false)
-
   return (
-    <>
-      <LoadingScreen onComplete={() => setLoaded(true)} />
+    <div className="app">
+      <PetalLayer active={true} />
+      <MusicToggle />
 
-      <div className="app">
-        <PetalLayer active={loaded} />
-        <MusicToggle />
+      <main className="scenes">
+        <SceneIntro />
+        <SceneJiangnan />
+        <SceneLife />
+        <SceneWishes />
+        <SceneLetter />
+      </main>
 
-        <main className="scenes">
-          <SceneIntro />
-          <SceneJiangnan />
-          <SceneLife />
-          <SceneWishes />
-          <SceneLetter />
-        </main>
-
-        <footer className="site-footer">
-          <p>愿这份小笺，陪你走过每一个温柔的日子</p>
-        </footer>
-      </div>
-    </>
+      <footer className="site-footer">
+        <p>愿这份小笺，陪你走过每一个温柔的日子</p>
+      </footer>
+    </div>
   )
 }
 
